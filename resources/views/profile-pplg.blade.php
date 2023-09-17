735182c82ba146cdb1c54f36ab47b2bf7c5c992d
RaihanFatur <raihanfatur16@gmail.com>
2023-09-17 12:12:19 +0700

    Tugas_Company_Profil_RaihanFatur_28_XIRPL2

---
+++ resources/views/profile-pplg.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styles.css">
+    <title>Company Profil</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="container">
+                <h1 class="logo">PPLG</h1>
+                <ul class="nav-list">
+                    <li><a href="#home">Beranda</a></li>
+                    <li><a href="#about">Materi</a></li>
+                    <li><a href="#services">Tentang Kami</a></li>
+                    <li><a href="/cv">CV</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Content sections -->
+    <section id="home">
+        <div class="ace">
+        <img src="/images/coding.png" width="250px" alt="">
+        <h2>PPLG</h2>
+        <p> Apa itu PPLG ? Jurusan yang mempelajari cara pengembangan perangkat lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan perangkat lunak dan manajemen kualitas.</p>
+</div>
+    </section>
+
+    <section id="about">
+    <section class="konten" id="section2">
+        <h1 class='judul'>Materi Jurusan PPLG</h1>
+        <h3 class='judull'>Berikut ini merupakan materi umum DI PPLG</h3>
+    <div class="content-container">
+        <div class="box">
+            <div class="box-img">
+                <img src="/images/htmlicon.png" alt="">
+            </div>
+            <h2>HTML</h2>
+            <span>
+            HTML adalah bahasa yang digunakan untuk membuat halaman web. 
+            Bagi yang berkecimpung di dunia perangkat gadget dan ilmu komputer, 
+            pasti sudah tidak asing lagi dengan berbagai kode di dalamnya. 
+            HTML adalah kependekan dari Hypertext Markup Language.
+            </span>
+        </div>
+
+        <div class="box">
+            <div class="box-img">
+                <img src="/images/cssicon.png" alt="">
+            </div>
+            <h2>CSS</h2>
+            <span>
+            CSS adalah bahasa Cascading Style Sheet dan biasanya digunakan untuk mengatur 
+            tampilan elemen yang tertulis dalam bahasa markup, seperti HTML. 
+            CSS berfungsi untuk memisahkan konten dari tampilan visualnya di situs.
+            </span>
+        </div>
+
+        <div class="box">
+            <div class="box-img">
+                <img src="/images/phpicon.png" alt="">
+            </div>
+            <h2>PHP</h2>
+            <span>
+            Hypertext Preprocessor atau PHP adalah bahasa penulisan skrip open-source 
+            yang banyak digunakan dalam pemrograman atau pengembangan website (web development). 
+            Bahasa ini umumnya dijalankan dalam komunikasi sisi server, dan saat ini didukung oleh hampir semua sistem.
+            </span>
+        </div>
+    </div>
+    </section>
+
+        
+    </section>
+
+    <section id="services">
+        <h2>Tentang Kami</h2>
+        <p class="cs">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos incidunt laudantium consequuntur, omnis enim ea quibusdam, perspiciatis corporis corrupti dolorem modi, illum voluptates voluptatem in exercitationem consectetur magnam optio. Id.</p>
+    </section>
+</body>
+</html>
